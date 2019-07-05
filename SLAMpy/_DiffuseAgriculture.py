dbc90105c4fb646bc763126ad7c3dae90c796825
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2019-07-05 16:59:51 +0200

    Moving geoprocessing into separate function
    In order to be callable from Python scripts without requiring an instance of the class CCTv2, the geoprocessing part of the execute method is extracted and put into a separate function in the same module.

--- SLAMpy/_DiffuseAgriculture.py
+++ SLAMpy/_DiffuseAgriculture.py
@@ -65,75 +65,86 @@ class CCTv2(object):
         return [nutrient, region, selection, in_arable, in_pasture, out_arable, out_pasture]
 
     def execute(self, parameters, messages):
-        """
-        :param parameters: list of the 8 parameters in the order as follows:
-           [0] nutrient of interest [type: str] {possible values: 'Nitrogen (N)' or 'Nitrogen (P)'}
-           [1] path of the feature class for the region of interest [type: str] {required}
-           [2] SQL query to select specific location(s) within region [type: str] {optional}
-           [3] path of the input feature class of the CCT data for arable [type: str] {required}
-           [4] path of the input feature class of the CCT data for pasture [type: str] {required}
-           [5] path of the output feature class for arable nutrient load [type: str] {required}
-           [6] path of the output feature class for pasture nutrient load [type: str] {required}
-        :param messages: Messages object provided by ArcPy when running the tool
-
-        N.B. If the optional parameters are not used, they must be set to None.
-        """
-
         # retrieve parameters
         nutrient, region, selection, in_arable, in_pasture, out_arable, out_pasture = \
             [p.valueAsText for p in parameters]
 
-        # determine which nutrient to work on
-        nutrient = 'N' if nutrient == 'Nitrogen (N)' else 'P'
+        # run geoprocessing function
+        cct_v2_geoprocessing(nutrient, region, selection, in_arable, in_pasture, out_arable, out_pasture, messages)
 
-        # determine which location to work on
-        arcpy.MakeFeatureLayer_management(region, 'lyrLocation')
-        if selection:  # i.e. selection requested
-            messages.addMessage("Selecting requested Location(s) within Region")
-            arcpy.SelectLayerByAttribute_management('lyrLocation', "NEW_SELECTION", selection)
 
-        # calculate load for arable
-        messages.addMessage("Calculating {} load for arable.".format(nutrient))
+def cct_v2_geoprocessing(nutrient, region, selection, in_arable, in_pasture, out_arable, out_pasture, messages):
+    """
+    :param nutrient: nutrient of interest {possible values: 'Nitrogen (N)' or 'Nitrogen (P)'} [required]
+    :type nutrient: str
+    :param region: path of the feature class for the region of interest [required]
+    :type region: str
+    :param selection: SQL query to select specific location(s) within region [optional]
+    :type selection: str
+    :param in_arable: path of the input feature class of the CCT data for arable [required]
+    :type in_arable: str
+    :param in_pasture: path of the input feature class of the CCT data for pasture [required]
+    :type in_pasture: str
+    :param out_arable: path of the output feature class for arable nutrient load [required]
+    :type out_arable: str
+    :param out_pasture: path of the output feature class for pasture nutrient load [required]
+    :type out_pasture: str
+    :param messages: object used for communication with the user interface [required]
+    :type messages: instance of a class featuring a 'addMessage' method
 
-        arcpy.Intersect_analysis(['lyrLocation', in_arable], out_arable,
-                                 join_attributes="ALL", output_type="INPUT")
+    N.B. If the optional parameters are not used, they must be set to None.
+    """
+    # determine which nutrient to work on
+    nutrient = 'N' if nutrient == 'Nitrogen (N)' else 'P'
 
-        arcpy.AddField_management(out_arable, "Area_ha", "DOUBLE",
-                                  field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
-        arcpy.CalculateField_management(out_arable, "Area_ha", "!shape.area@hectares!",
-                                        expression_type="PYTHON_9.3")
+    # determine which location to work on
+    arcpy.MakeFeatureLayer_management(region, 'lyrLocation')
+    if selection:  # i.e. selection requested
+        messages.addMessage("Selecting requested Location(s) within Region")
+        arcpy.SelectLayerByAttribute_management('lyrLocation', "NEW_SELECTION", selection)
 
-        arcpy.AddField_management(out_arable, "GWCrop2CCT", "DOUBLE",
-                                  field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
-        arcpy.CalculateField_management(out_arable, "GWCrop2CCT",
-                                        "!{}SwFromGw! * !Area_ha!".format(nutrient.lower()),
-                                        expression_type="PYTHON_9.3")
+    # calculate load for arable
+    messages.addMessage("Calculating {} load for arable.".format(nutrient))
 
-        arcpy.AddField_management(out_arable, "Crop2CCT", "DOUBLE",
-                                  field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
-        arcpy.CalculateField_management(out_arable, "Crop2CCT",
-                                        "!{}TotaltoSWreceptor! * !Area_ha!".format(nutrient.lower()),
-                                        expression_type="PYTHON_9.3")
+    arcpy.Intersect_analysis(['lyrLocation', in_arable], out_arable,
+                             join_attributes="ALL", output_type="INPUT")
 
-        # calculate load for pasture
-        messages.addMessage("Calculating {} load for pasture.".format(nutrient))
+    arcpy.AddField_management(out_arable, "Area_ha", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_arable, "Area_ha", "!shape.area@hectares!",
+                                    expression_type="PYTHON_9.3")
 
-        arcpy.Intersect_analysis(['lyrLocation', in_pasture], out_pasture,
-                                 join_attributes="ALL", output_type="INPUT")
+    arcpy.AddField_management(out_arable, "GWCrop2CCT", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_arable, "GWCrop2CCT",
+                                    "!{}SwFromGw! * !Area_ha!".format(nutrient.lower()),
+                                    expression_type="PYTHON_9.3")
 
-        arcpy.AddField_management(out_pasture, "Area_ha", "DOUBLE",
-                                  field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
-        arcpy.CalculateField_management(out_pasture, "Area_ha", "!shape.area@hectares!",
-                                        expression_type="PYTHON_9.3")
+    arcpy.AddField_management(out_arable, "Crop2CCT", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_arable, "Crop2CCT",
+                                    "!{}TotaltoSWreceptor! * !Area_ha!".format(nutrient.lower()),
+                                    expression_type="PYTHON_9.3")
 
-        arcpy.AddField_management(out_pasture, "GWPast2CCT", "DOUBLE",
-                                  field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
-        arcpy.CalculateField_management(out_pasture, "GWPast2CCT",
-                                        "!{}SwFromGw! * !Area_ha!".format(nutrient.lower()),
-                                        expression_type="PYTHON_9.3")
+    # calculate load for pasture
+    messages.addMessage("Calculating {} load for pasture.".format(nutrient))
 
-        arcpy.AddField_management(out_pasture, "Past2CCT", "DOUBLE",
-                                  field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
-        arcpy.CalculateField_management(out_pasture, "Past2CCT",
-                                        "!{}TotaltoSWreceptor! * !Area_ha!".format(nutrient.lower()),
-                                        expression_type="PYTHON_9.3")
+    arcpy.Intersect_analysis(['lyrLocation', in_pasture], out_pasture,
+                             join_attributes="ALL", output_type="INPUT")
+
+    arcpy.AddField_management(out_pasture, "Area_ha", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_pasture, "Area_ha", "!shape.area@hectares!",
+                                    expression_type="PYTHON_9.3")
+
+    arcpy.AddField_management(out_pasture, "GWPast2CCT", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_pasture, "GWPast2CCT",
+                                    "!{}SwFromGw! * !Area_ha!".format(nutrient.lower()),
+                                    expression_type="PYTHON_9.3")
+
+    arcpy.AddField_management(out_pasture, "Past2CCT", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_pasture, "Past2CCT",
+                                    "!{}TotaltoSWreceptor! * !Area_ha!".format(nutrient.lower()),
+                                    expression_type="PYTHON_9.3")
