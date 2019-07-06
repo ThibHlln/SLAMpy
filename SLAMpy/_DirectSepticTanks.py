1ef0355dd04bcab54b23ce7a82e753c257f0e260
Thibault Hallouin <thibault.hallouin@ucdconnect.ie>
2019-07-06 13:58:41 +0200

    Adding septic tanks tool

---
+++ SLAMpy/_DirectSepticTanks.py
@@ -0,0 +1,141 @@
+from os import path, sep
+import arcpy
+
+
+class DWTSv2(object):
+    def __init__(self):
+        self.__version__ = '2'
+        self.category = 'Sources'
+        self.label = 'Septic Tanks [v{}]'.format(self.__version__)
+        self.description = "Direct nutrient discharges from septic tank systems (based on SANICOSE model)."
+        self.canRunInBackground = False
+
+    def getParameterInfo(self):
+        # Define Workspace
+        root = path.dirname(path.dirname(path.realpath(__file__)))
+        arcpy.env.workspace = root
+
+        # Parameters for Folders Options
+        in_gdb = sep.join([root, 'in', 'input.gdb'])
+
+        in_fld = sep.join([root, 'in'])
+
+        out_gdb = arcpy.Parameter(
+            displayName="Output Geodatabase",
+            name="out_gdb",
+            datatype="DEWorkspace",
+            parameterType="Required",
+            direction="Input",
+            category='# Folders Options')
+        out_gdb.value = sep.join([root, 'out', 'output.gdb'])
+
+        out_fld = arcpy.Parameter(
+            displayName="Output Folder",
+            name="out_fld",
+            datatype="DEWorkspace",
+            parameterType="Required",
+            direction="Input",
+            category='# Folders Options')
+        out_fld.value = sep.join([root, 'out'])
+
+        # Parameters Common to All Sources
+        project_name = arcpy.Parameter(
+            displayName="Name of the Project",
+            name="project_name",
+            datatype="GPString",
+            parameterType="Required",
+            direction="Input")
+
+        nutrient = arcpy.Parameter(
+            displayName="Nutrient of Interest",
+            name="nutrient",
+            datatype="GPString",
+            parameterType="Required",
+            direction="Input")
+        nutrient.filter.type = "ValueList"
+        nutrient.filter.list = ['Nitrogen (N)', 'Phosphorus (P)']
+
+        region = arcpy.Parameter(
+            displayName="Region of Interest",
+            name="region",
+            datatype="DEFeatureClass",
+            parameterType="Required",
+            direction="Input")
+
+        selection = arcpy.Parameter(
+            displayName="Selection within Region",
+            name="selection",
+            datatype="GPSQLExpression",
+            parameterType="Optional",
+            direction="Input")
+        selection.parameterDependencies = [region.name]
+
+        # Parameters specific to Septic Tanks
+        in_dwts = arcpy.Parameter(
+            displayName="Domestic Septic Tanks Data",
+            name="in_dwts",
+            datatype="DEFeatureClass",
+            parameterType="Required",
+            direction="Input",
+            category="Septic Tanks Data Options")
+        in_dwts.value = sep.join([in_gdb, 'SepticTanks_Discharge'])
+
+        return [out_gdb, out_fld,
+                project_name, nutrient, region, selection,
+                in_dwts]
+
+    def execute(self, parameters, messages):
+        """
+        :param parameters: list of the 8 parameters in the order as follows:
+           [0] nutrient of interest [type: str] {possible values: 'Nitrogen (N)' or 'Nitrogen (P)'}
+           [1] path of the feature class for the region of interest [type: str] {required}
+           [2] SQL query to select specific location(s) within region [type: str] {optional}
+           [3] path of the input feature class of the domestic septic tanks data for arable [type: str] {required}
+           [4] path of the output feature class for Septic Tanks load [type: str] {required}
+        :param messages: Messages object provided by ArcPy when running the tool
+
+        N.B. If the optional parameters are not used, they must be set to None.
+        """
+
+        # retrieve parameters
+        out_gdb, out_fld, project_name, nutrient, region, selection, in_dwts = \
+            [p.valueAsText for p in parameters]
+
+        # determine which nutrient to work on
+        nutrient = 'N' if nutrient == 'Nitrogen (N)' else 'P'
+
+        # determine which location to work on
+        if selection:  # i.e. selection requested
+            messages.addMessage("> Selecting requested Location(s) within Region.")
+            location = sep.join([out_gdb, project_name + '_SelectedRegion'])
+            arcpy.Select_analysis(region, location, selection)
+        else:
+            location = region
+
+        # run geoprocessing function
+        dwts_v2_geoprocessing(project_name, nutrient, location, in_dwts, out_gdb, messages)
+
+
+def dwts_v2_geoprocessing(project_name, nutrient, location, in_dwts, out_gdb, messages,
+                          out_dwts=None):
+
+    # calculate load for septic tank systems
+    messages.addMessage("> Calculating {} load for septic tank systems.".format(nutrient))
+
+    if not out_dwts:
+        out_dwts = sep.join([out_gdb, project_name + '_{}_SepticTanks'.format(nutrient)])
+
+    arcpy.Intersect_analysis([location, in_dwts], out_dwts,
+                             join_attributes="ALL", output_type="INPUT")
+
+    arcpy.AddField_management(out_dwts, "GW_DWTS", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_dwts, "GW_DWTS",
+                                    "!GW_{}_2c!".format(nutrient),
+                                    expression_type="PYTHON_9.3")
+
+    arcpy.AddField_management(out_dwts, "DWTS", "DOUBLE",
+                              field_is_nullable="NULLABLE", field_is_required="NON_REQUIRED")
+    arcpy.CalculateField_management(out_dwts, "DWTS",
+                                    "!Total_{}_2c!".format(nutrient),
+                                    expression_type="PYTHON_9.3")
